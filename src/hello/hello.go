925e2549439e7024f2d35ccba12ba7baaa0c5963
ShuoyuGong <2788696894@qq.com>
2021-02-10 21:12:01 +0800

    2021-02-10

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var a = 6
-	var b = 11
-	fmt.Println(a & b)
+Lable1:
+	for {
+		for a := 0; a < 10; a++ {
+			fmt.Println(a)
+			if a > 5 {
+				break Lable1
+			}
+		}
+	}
+	fmt.Println("结束了")
 }
